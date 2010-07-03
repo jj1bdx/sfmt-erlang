fdeb60443483f0a72a50e4983a606b58743ccbd6
Kenji Rikitake <kenji.rikitake@acm.org>
2010-07-03 12:57:02 +0859

    added do_recursion/4 (untested)

--- sfmt.erl
+++ sfmt.erl
@@ -4,7 +4,8 @@
 
 -export([
 	 rshift128/2,
-	 lshift128/2
+	 lshift128/2,
+	 do_recursion/4
 	 ]).
 
 -include("sfmt.hrl").
@@ -42,3 +43,22 @@ lshift128(In, Shift) ->
 	band ?BITMASK64,
     [OL band ?BITMASK32, OL bsr 32, 
      OH band ?BITMASK32, OH bsr 32].
+
+do_recursion(A, B, C, D) ->
+    [A0, A1, A2, A3] = A,
+    [B0, B1, B2, B3] = B,
+    % [C0, C1, C2, C3] = C,
+    [D0, D1, D2, D3] = D,
+    [X0, X1, X2, X3] = lshift128(A, ?SL2),
+    [Y0, Y1, Y2, Y3] = rshift128(C, ?SR2),
+    [
+     A0 bxor X0 bxor ((B0 bsr ?SR1) band ?MSK1) bxor Y0
+        bxor ((D0 bsl ?SL1) band ?BITMASK32),
+     A1 bxor X1 bxor ((B1 bsr ?SR1) band ?MSK1) bxor Y1
+        bxor ((D1 bsl ?SL1) band ?BITMASK32),
+     A2 bxor X2 bxor ((B2 bsr ?SR1) band ?MSK1) bxor Y2
+        bxor ((D2 bsl ?SL1) band ?BITMASK32),
+     A3 bxor X3 bxor ((B3 bsr ?SR1) band ?MSK1) bxor Y3
+        bxor ((D3 bsl ?SL1) band ?BITMASK32)
+     ].
+
