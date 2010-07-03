a63e270bb76ec0c60b4063d93d11e5a968028c42
Kenji Rikitake <kenji.rikitake@acm.org>
2010-07-03 11:10:27 +0859

    initial M19937 params added to sfmt.htl

---
+++ sfmt.hrl
@@ -0,0 +1,37 @@
+%% Common SFMT parameters for sfmt-erlang
+
+%% Mersenne Exponent. The period of the sequence 
+%%  is a multiple of 2^MEXP-1.
+-define(MEXP, 19937).
+%% SFMT generator has an internal state array of 128-bit integers,
+%% and N is its size.
+-define(N, ((MEXP / 128) + 1)).
+%% N32 is the size of internal state array when regarded as an array
+%% of 32-bit integers.
+-define(N32, (N * 4)).
+%% the pick up position of the array.
+-define(POS1, 122).
+%% the parameter of shift left as four 32-bit registers.
+-define(SL1, 18).
+%% the parameter of shift left as one 128-bit register. 
+%% The 128-bit integer is shifted by (SL2 * 8) bits.
+-define(SL2, 1).
+%% the parameter of shift right as four 32-bit registers.
+-define(SR1, 11).
+%% the parameter of shift right as one 128-bit register. 
+%% The 128-bit integer is shifted by (SL2 * 8) bits.
+-define(SR2, 1).
+%% A bitmask, used in the recursion.  These parameters are introduced
+%% to break symmetry of SIMD.
+-define(MSK1, 16#dfffffef).
+-define(MSK2, 16#ddfecb7f).
+-define(MSK3, 16#bffaffff).
+-define(MSK4, 16#bffffff6).
+%% These definitions are part of a 128-bit period certification vector.
+-define(PARITY1, 16#00000001).
+-define(PARITY2, 16#00000000).
+-define(PARITY3, 16#00000000).
+-define(PARITY4, 16#13c9e684).
+%% identification string for the algorithm
+-define(IDSTR, "SFMT-19937:122-18-1-11-1:dfffffef-ddfecb7f-bffaffff-bffffff6").
+
