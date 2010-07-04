f9fd1bcb6732bead8411284ab36863d35d778502
Kenji Rikitake <kenji.rikitake@acm.org>
2010-07-04 12:38:56 +0859

    removed sfmt.hrl and merged into sfmt.erl

--- sfmt.erl
+++ sfmt.erl
@@ -3,21 +3,76 @@
 -module(sfmt).
 
 -export([
-	 rshift128/2,
-	 lshift128/2,
-	 do_recursion/4,
 	 gen_rand_all/1,
 	 gen_rand_list32/2,
-	 period_certification/1,
 	 get_idstring/0,
 	 get_min_array_size32/0,
-	 func1/1,
-	 func2/1,
 	 init_gen_rand/1,
-	 init_by_list32/1
+	 init_by_list32/1,
+	 gen_rand32/2
 	 ]).
 
--include("sfmt.hrl").
+%% Common SFMT parameters for sfmt-erlang
+
+%% SFMT period parameters
+%% details on SFMT-1.3.3 source code
+
+%% Mersenne Exponent. The period of the sequence 
+%%  is a multiple of 2^MEXP-1.
+-define(MEXP, 19937).
+%% SFMT generator has an internal state array of 128-bit integers,
+%% and N is its size.
+-define(N, ((?MEXP div 128) + 1)).
+%% N32 is the size of internal state array when regarded as an array
+%% of 32-bit integers.
+-define(N32, (?N * 4)).
+%% the pick up position of the array.
+-define(POS1, 122).
+%% the parameter of shift left as four 32-bit registers.
+-define(SL1, 18).
+%% the parameter of shift left as one 128-bit register. 
+%% The 128-bit integer is shifted by (SL2 * 8) bits.
+-define(SL2, 1).
+%% the parameter of shift right as four 32-bit registers.
+-define(SR1, 11).
+%% the parameter of shift right as one 128-bit register. 
+%% The 128-bit integer is shifted by (SL2 * 8) bits.
+-define(SR2, 1).
+%% A bitmask, used in the recursion.  These parameters are introduced
+%% to break symmetry of SIMD.
+-define(MSK1, 16#dfffffef).
+-define(MSK2, 16#ddfecb7f).
+-define(MSK3, 16#bffaffff).
+-define(MSK4, 16#bffffff6).
+%% These definitions are part of a 128-bit period certification vector.
+-define(PARITY1, 16#00000001).
+-define(PARITY2, 16#00000000).
+-define(PARITY3, 16#00000000).
+-define(PARITY4, 16#13c9e684).
+%% identification string for the algorithm
+-define(IDSTR, "SFMT-19937:122-18-1-11-1:dfffffef-ddfecb7f-bffaffff-bffffff6").
+
+%% SFMT calculation masks
+
+-define(BITMASK32, 16#ffffffff).
+-define(BITMASK64, 16#ffffffffffffffff).
+
+%% for init_by_list32/1:
+%% Lag =
+%% 	if
+%% 	    ?N32 >= 623 ->
+%% 		11;
+%% 	    ?N32 >= 68 ->
+%% 		7;
+%% 	    ?N32 >= 39 ->
+%% 		5;
+%% 	    ?N32 ->
+%% 		3
+%% 	end,
+%% Mid = (?N32 - Lag) div 2
+
+-define(Lag, 11).
+-define(Mid, 306).
 
 %% internal state format: 
 %% list of 32-bit unsigned ints,
@@ -300,4 +355,12 @@ init_by_list32(Key) ->
     period_certification(
       array:to_list(
 	init_by_list32_rec2(?N32, I1, A5))).
+
+gen_rand32([], Int) ->
+    Int2 = gen_rand_all(Int),
+    [H|T] = Int2,
+    {H, T, Int2};
+gen_rand32(Randlist, Int) ->
+    [H|T] = Randlist,
+    {H, T, Int}.
 
