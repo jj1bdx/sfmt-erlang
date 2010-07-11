d9ebc72f50986b569d408a47e20845072584dd36
Kenji Rikitake <kenji.rikitake@acm.org>
2010-07-11 16:12:54 +0859

    removed c_src/compile.sh

--- c_src/compile.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-CFLAGS="-O3 -finline-functions -fomit-frame-pointer -fno-strict-aliasing --param max-inline-insns-single=1800 --param inline-unit-growth=500 --param large-function-growth=900 -Wmissing-prototypes -Wall -std=c99"; export CFLAGS
-#
-gcc44 $CFLAGS -fPIC -shared -o sfmt_nif.so sfmt_nif.c -I /usr/local/lib/erlang/usr/include/
