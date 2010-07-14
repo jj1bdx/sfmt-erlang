2b8ed90c7748ed11baaca19a8ecdacde874a167a
Kenji Rikitake <kenji.rikitake@acm.org>
2010-07-14 14:23:22 +0859

    Added the Kyoto University's credits to the source code, license, and README

--- c_src/sfmt_nif.c
+++ c_src/sfmt_nif.c
@@ -1,26 +1,28 @@
 /* sfmt_nif.c: C NIF SFMT API functions for Erlang */
 /* based on SFMT-1.3.3 and sfmt-extstate 0.1.0_RELEASE */
 /* 
-   Copyright (c) 2010 Kenji Rikitake. All rights reserved.
 
+   Copyright (c) 2010 Kenji Rikitake and Kyoto University. All rights
+   reserved.
+   
    Copyright (c) 2006,2007 Mutsuo Saito, Makoto Matsumoto and Hiroshima
    University. All rights reserved.
-
+   
    Redistribution and use in source and binary forms, with or without
    modification, are permitted provided that the following conditions are
    met:
-
-   * Redistributions of source code must retain the above copyright
-   notice, this list of conditions and the following disclaimer.
-   * Redistributions in binary form must reproduce the above
-   copyright notice, this list of conditions and the following
-   disclaimer in the documentation and/or other materials provided
-   with the distribution.
-   * Neither the name of the Hiroshima University nor the names of
-   its contributors may be used to endorse or promote products
-   derived from this software without specific prior written
-   permission.
-
+   
+       * Redistributions of source code must retain the above copyright
+         notice, this list of conditions and the following disclaimer.
+       * Redistributions in binary form must reproduce the above
+         copyright notice, this list of conditions and the following
+         disclaimer in the documentation and/or other materials provided
+         with the distribution.
+       * Neither the names of the Hiroshima University and the Kyoto
+         University nor the names of its contributors may be used to
+         endorse or promote products derived from this software without
+         specific prior written permission.
+   
    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
    "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
    LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
@@ -32,6 +34,7 @@
    THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
    (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
    OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
  */
 
 #include <stdlib.h>
