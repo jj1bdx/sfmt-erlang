d286e406c69dde80ce05d9c16656e2bbf02e8c3a
Kenji Rikitake <kenji.rikitake@acm.org>
2010-07-16 11:14:24 +0859

    removed c_src/sfmt_nif.h and merged the contents into c_src/sfmt_nif.c; also SFMT parameter macros modified (by adding parentheses to the raw integers)

--- c_src/sfmt_nif.c
+++ c_src/sfmt_nif.c
@@ -42,7 +42,97 @@
 #include <string.h>
 
 #include "erl_nif.h"
-#include "sfmt_nif.h"
+
+#if defined(__STDC_VERSION__) && (__STDC_VERSION__ >= 199901L)
+  #include <inttypes.h>
+#elif defined(_MSC_VER) || defined(__BORLANDC__)
+  typedef unsigned int uint32_t;
+  typedef unsigned __int64 uint64_t;
+  #define inline __inline
+#else
+  #include <inttypes.h>
+  #if defined(__GNUC__)
+    #define inline __inline__
+  #endif
+#endif
+
+#ifndef PRIu64
+  #if defined(_MSC_VER) || defined(__BORLANDC__)
+    #define PRIu64 "I64u"
+    #define PRIx64 "I64x"
+  #else
+    #define PRIu64 "llu"
+    #define PRIx64 "llx"
+  #endif
+#endif
+
+#if defined(__GNUC__)
+#define ALWAYSINLINE __attribute__((always_inline))
+#else
+#define ALWAYSINLINE
+#endif
+
+#if defined(_MSC_VER)
+  #if _MSC_VER >= 1200
+    #define PRE_ALWAYS __forceinline
+  #else
+    #define PRE_ALWAYS inline
+  #endif
+#else
+  #define PRE_ALWAYS inline
+#endif
+
+/* 128-bit SIMD data type for standard C */
+
+/** 128-bit data structure */
+struct W128_T {
+    uint32_t u[4];
+};
+/** 128-bit data type */
+typedef struct W128_T w128_t;
+
+/* 
+   SFMT parameter section
+   Other sets of parameters are available from SFMT-1.3.3
+   source code.
+ */
+
+/** Mersenne Exponent. The period of the sequence 
+ *  is a multiple of 2^MEXP-1. */
+#define MEXP (19937)
+/** SFMT generator has an internal state array of 128-bit integers,
+ * and N is its size. */
+#define N (156) /* ((MEXP / 128) + 1) */
+/** N32 is the size of internal state array when regarded as an array
+ * of 32-bit integers.*/
+#define N32 (624) /* (N * 4) */
+/** the pick up position of the array. */
+#define POS1 (122) 
+/** the parameter of shift left as four 32-bit registers. */
+#define SL1 (18)
+/** the parameter of shift left as one 128-bit register. 
+ * The 128-bit integer is shifted by (SL2 * 8) bits. */
+#define SL2 (1)
+/** the parameter of shift right as four 32-bit registers. */
+#define SR1 (11)
+/** the parameter of shift right as one 128-bit register. 
+ * The 128-bit integer is shifted by (SL2 * 8) bits. */
+#define SR2 (1)
+/** A bitmask, used in the recursion.  These parameters are introduced
+ * to break symmetry of SIMD. */
+#define MSK1 (0xdfffffefU)
+#define MSK2 (0xddfecb7fU)
+#define MSK3 (0xbffaffffU)
+#define MSK4 (0xbffffff6U)
+/** These definitions are part of a 128-bit period certification vector. */
+#define PARITY1	(0x00000001U)
+#define PARITY2	(0x00000000U)
+#define PARITY3	(0x00000000U)
+#define PARITY4	(0x13c9e684U)
+/* identification string for the algorithm */
+#define IDSTR	"SFMT-19937:122-18-1-11-1:dfffffef-ddfecb7f-bffaffff-bffffff6"
+/* float multiplier */
+#define FLOAT_CONST (1.0/4294967295.0) 
 
 /* prototypes */
 static int load(ErlNifEnv *env, void **priv_data, ERL_NIF_TERM load_info);
