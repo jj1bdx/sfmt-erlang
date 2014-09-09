6262ffe4ec243b8656eb242f904e2d811e65f7fe
Kenji Rikitake <kenji.rikitake@acm.org>
2014-09-09 20:42:30 +0900

    Update erlang.mk (reverted)

--- erlang.mk
+++ erlang.mk
@@ -251,278 +251,68 @@ clean-app:
 # Copyright (c) 2014, Loïc Hoguin <essen@ninenines.eu>
 # This file is part of erlang.mk and subject to the terms of the ISC License.
 
-.PHONY: bootstrap bootstrap-lib bootstrap-rel new list-templates
+.PHONY: clean-c_src
+# todo
 
-# Core targets.
+# Configuration.
 
-help::
-	@printf "%s\n" "" \
-		"Bootstrap targets:" \
-		"  bootstrap          Generate a skeleton of an OTP application" \
-		"  bootstrap-lib      Generate a skeleton of an OTP library" \
-		"  bootstrap-rel      Generate the files needed to build a release" \
-		"  new t=TPL n=NAME   Generate a module NAME based on the template TPL" \
-		"  list-templates     List available templates"
+C_SRC_DIR = $(CURDIR)/c_src
+C_SRC_ENV ?= $(C_SRC_DIR)/env.mk
+C_SRC_OPTS ?=
+C_SRC_OUTPUT ?= $(CURDIR)/priv/$(PROJECT).so
 
-# Bootstrap templates.
+# System type and C compiler/flags.
 
-bs_appsrc = "{application, $(PROJECT), [" \
-	"	{description, \"\"}," \
-	"	{vsn, \"0.1.0\"}," \
-	"	{modules, []}," \
-	"	{registered, []}," \
-	"	{applications, [" \
-	"		kernel," \
-	"		stdlib" \
-	"	]}," \
-	"	{mod, {$(PROJECT)_app, []}}," \
-	"	{env, []}" \
-	"]}."
-bs_appsrc_lib = "{application, $(PROJECT), [" \
-	"	{description, \"\"}," \
-	"	{vsn, \"0.1.0\"}," \
-	"	{modules, []}," \
-	"	{registered, []}," \
-	"	{applications, [" \
-	"		kernel," \
-	"		stdlib" \
-	"	]}" \
-	"]}."
-bs_Makefile = "PROJECT = $(PROJECT)" \
-	"include erlang.mk"
-bs_app = "-module($(PROJECT)_app)." \
-	"-behaviour(application)." \
-	"" \
-	"-export([start/2])." \
-	"-export([stop/1])." \
-	"" \
-	"start(_Type, _Args) ->" \
-	"	$(PROJECT)_sup:start_link()." \
-	"" \
-	"stop(_State) ->" \
-	"	ok."
-bs_relx_config = "{release, {$(PROJECT)_release, \"1\"}, [$(PROJECT)]}." \
-	"{extended_start_script, true}." \
-	"{sys_config, \"rel/sys.config\"}." \
-	"{vm_args, \"rel/vm.args\"}."
-bs_sys_config = "[" \
-	"]."
-bs_vm_args = "-name $(PROJECT)@127.0.0.1" \
-	"-setcookie $(PROJECT)" \
-	"-heart"
-# Normal templates.
-tpl_supervisor = "-module($(n))." \
-	"-behaviour(supervisor)." \
-	"" \
-	"-export([start_link/0])." \
-	"-export([init/1])." \
-	"" \
-	"start_link() ->" \
-	"	supervisor:start_link({local, ?MODULE}, ?MODULE, [])." \
-	"" \
-	"init([]) ->" \
-	"	Procs = []," \
-	"	{ok, {{one_for_one, 1, 5}, Procs}}."
-tpl_gen_server = "-module($(n))." \
-	"-behaviour(gen_server)." \
-	"" \
-	"%% API." \
-	"-export([start_link/0])." \
-	"" \
-	"%% gen_server." \
-	"-export([init/1])." \
-	"-export([handle_call/3])." \
-	"-export([handle_cast/2])." \
-	"-export([handle_info/2])." \
-	"-export([terminate/2])." \
-	"-export([code_change/3])." \
-	"" \
-	"-record(state, {" \
-	"})." \
-	"" \
-	"%% API." \
-	"" \
-	"-spec start_link() -> {ok, pid()}." \
-	"start_link() ->" \
-	"	gen_server:start_link(?MODULE, [], [])." \
-	"" \
-	"%% gen_server." \
-	"" \
-	"init([]) ->" \
-	"	{ok, \#state{}}." \
-	"" \
-	"handle_call(_Request, _From, State) ->" \
-	"	{reply, ignored, State}." \
-	"" \
-	"handle_cast(_Msg, State) ->" \
-	"	{noreply, State}." \
-	"" \
-	"handle_info(_Info, State) ->" \
-	"	{noreply, State}." \
-	"" \
-	"terminate(_Reason, _State) ->" \
-	"	ok." \
-	"" \
-	"code_change(_OldVsn, State, _Extra) ->" \
-	"	{ok, State}."
-tpl_cowboy_http = "-module($(n))." \
-	"-behaviour(cowboy_http_handler)." \
-	"" \
-	"-export([init/3])." \
-	"-export([handle/2])." \
-	"-export([terminate/3])." \
-	"" \
-	"-record(state, {" \
-	"})." \
-	"" \
-	"init(_, Req, _Opts) ->" \
-	"	{ok, Req, \#state{}}." \
-	"" \
-	"handle(Req, State=\#state{}) ->" \
-	"	{ok, Req2} = cowboy_req:reply(200, Req)," \
-	"	{ok, Req2, State}." \
-	"" \
-	"terminate(_Reason, _Req, _State) ->" \
-	"	ok."
-tpl_cowboy_loop = "-module($(n))." \
-	"-behaviour(cowboy_loop_handler)." \
-	"" \
-	"-export([init/3])." \
-	"-export([info/3])." \
-	"-export([terminate/3])." \
-	"" \
-	"-record(state, {" \
-	"})." \
-	"" \
-	"init(_, Req, _Opts) ->" \
-	"	{loop, Req, \#state{}, 5000, hibernate}." \
-	"" \
-	"info(_Info, Req, State) ->" \
-	"	{loop, Req, State, hibernate}." \
-	"" \
-	"terminate(_Reason, _Req, _State) ->" \
-	"	ok."
-tpl_cowboy_rest = "-module($(n))." \
-	"" \
-	"-export([init/3])." \
-	"-export([content_types_provided/2])." \
-	"-export([get_html/2])." \
-	"" \
-	"init(_, _Req, _Opts) ->" \
-	"	{upgrade, protocol, cowboy_rest}." \
-	"" \
-	"content_types_provided(Req, State) ->" \
-	"	{[{{<<\"text\">>, <<\"html\">>, '_'}, get_html}], Req, State}." \
-	"" \
-	"get_html(Req, State) ->" \
-	"	{<<\"<html><body>This is REST!</body></html>\">>, Req, State}."
-tpl_cowboy_ws = "-module($(n))." \
-	"-behaviour(cowboy_websocket_handler)." \
-	"" \
-	"-export([init/3])." \
-	"-export([websocket_init/3])." \
-	"-export([websocket_handle/3])." \
-	"-export([websocket_info/3])." \
-	"-export([websocket_terminate/3])." \
-	"" \
-	"-record(state, {" \
-	"})." \
-	"" \
-	"init(_, _, _) ->" \
-	"	{upgrade, protocol, cowboy_websocket}." \
-	"" \
-	"websocket_init(_, Req, _Opts) ->" \
-	"	Req2 = cowboy_req:compact(Req)," \
-	"	{ok, Req2, \#state{}}." \
-	"" \
-	"websocket_handle({text, Data}, Req, State) ->" \
-	"	{reply, {text, Data}, Req, State};" \
-	"websocket_handle({binary, Data}, Req, State) ->" \
-	"	{reply, {binary, Data}, Req, State};" \
-	"websocket_handle(_Frame, Req, State) ->" \
-	"	{ok, Req, State}." \
-	"" \
-	"websocket_info(_Info, Req, State) ->" \
-	"	{ok, Req, State}." \
-	"" \
-	"websocket_terminate(_Reason, _Req, _State) ->" \
-	"	ok."
-tpl_ranch_protocol = "-module($(n))." \
-	"-behaviour(ranch_protocol)." \
-	"" \
-	"-export([start_link/4])." \
-	"-export([init/4])." \
-	"" \
-	"-type opts() :: []." \
-	"-export_type([opts/0])." \
-	"" \
-	"-record(state, {" \
-	"	socket :: inet:socket()," \
-	"	transport :: module()" \
-	"})." \
-	"" \
-	"start_link(Ref, Socket, Transport, Opts) ->" \
-	"	Pid = spawn_link(?MODULE, init, [Ref, Socket, Transport, Opts])," \
-	"	{ok, Pid}." \
-	"" \
-	"-spec init(ranch:ref(), inet:socket(), module(), opts()) -> ok." \
-	"init(Ref, Socket, Transport, _Opts) ->" \
-	"	ok = ranch:accept_ack(Ref)," \
-	"	loop(\#state{socket=Socket, transport=Transport})." \
-	"" \
-	"loop(State) ->" \
-	"	loop(State)."
-
-# Plugin-specific targets.
-
-bootstrap:
-ifneq ($(wildcard src/),)
-	$(error Error: src/ directory already exists)
+UNAME_SYS := $(shell uname -s)
+ifeq ($(UNAME_SYS), Darwin)
+	CC ?= cc
+	CFLAGS ?= -O3 -std=c99 -arch x86_64 -flat_namespace -undefined suppress -finline-functions -Wall -Wmissing-prototypes
+else ifeq ($(UNAME_SYS), FreeBSD)
+	CC ?= cc
+	CFLAGS ?= -O3 -std=c99 -finline-functions -Wall -Wmissing-prototypes
+else ifeq ($(UNAME_SYS), Linux)
+	CC ?= gcc
+	CFLAGS ?= -O3 -std=c99 -finline-functions -Wall -Wmissing-prototypes
 endif
-	@printf "%s\n" $(bs_Makefile) > Makefile
-	@mkdir src/
-	@printf "%s\n" $(bs_appsrc) > src/$(PROJECT).app.src
-	@printf "%s\n" $(bs_app) > src/$(PROJECT)_app.erl
-	$(eval n := $(PROJECT)_sup)
-	@printf "%s\n" $(tpl_supervisor) > src/$(PROJECT)_sup.erl
 
-bootstrap-lib:
-ifneq ($(wildcard src/),)
-	$(error Error: src/ directory already exists)
-endif
-	@printf "%s\n" $(bs_Makefile) > Makefile
-	@mkdir src/
-	@printf "%s\n" $(bs_appsrc_lib) > src/$(PROJECT).app.src
+# Verbosity.
 
-bootstrap-rel:
-ifneq ($(wildcard relx.config),)
-	$(error Error: relx.config already exists)
-endif
-ifneq ($(wildcard rel/),)
-	$(error Error: rel/ directory already exists)
-endif
-	@printf "%s\n" $(bs_relx_config) > relx.config
-	@mkdir rel/
-	@printf "%s\n" $(bs_sys_config) > rel/sys.config
-	@printf "%s\n" $(bs_vm_args) > rel/vm.args
+c_src_verbose_0 = @echo " C_SRC " $(?F);
+c_src_verbose = $(appsrc_verbose_$(V))
 
-new:
-ifeq ($(wildcard src/),)
-	$(error Error: src/ directory does not exist)
-endif
-ifndef t
-	$(error Usage: make new t=TEMPLATE n=NAME)
-endif
-ifndef tpl_$(t)
-	$(error Unknown template)
-endif
-ifndef n
-	$(error Usage: make new t=TEMPLATE n=NAME)
-endif
-	@printf "%s\n" $(tpl_$(t)) > src/$(n).erl
+# Targets.
 
-list-templates:
-	@echo Available templates: $(sort $(patsubst tpl_%,%,$(filter tpl_%,$(.VARIABLES))))
+ifeq ($(wildcard $(C_SRC_DIR)/Makefile),)
+
+app:: $(C_SRC_ENV)
+	@mkdir -p priv/
+	$(c_src_verbose) $(CC) $(CFLAGS) $(C_SRC_DIR)/*.c -fPIC -shared -o $(C_SRC_OUTPUT) \
+		-I $(ERTS_INCLUDE_DIR) $(C_SRC_OPTS)
+
+$(C_SRC_ENV):
+	erl -noshell -noinput -eval "file:write_file(\"$(C_SRC_ENV)\", \
+		io_lib:format(\"ERTS_INCLUDE_DIR ?= ~s/erts-~s/include/\", \
+			[code:root_dir(), erlang:system_info(version)])), \
+		init:stop()."
+
+-include $(C_SRC_ENV)
+
+else
+ifneq ($(wildcard $(C_SRC_DIR),))
+
+app::
+	$(MAKE) -C $(C_SRC_DIR)
+
+clean::
+	$(MAKE) -C $(C_SRC_DIR) clean
+
+endif
+endif
+
+clean:: clean-c_src
+
+clean-c_src:
+	$(gen_verbose) rm -f $(C_SRC_ENV) $(C_SRC_OUTPUT)
 
 # Copyright (c) 2013-2014, Loïc Hoguin <essen@ninenines.eu>
 # This file is part of erlang.mk and subject to the terms of the ISC License.
@@ -646,32 +436,6 @@ endif
 # Copyright (c) 2013-2014, Loïc Hoguin <essen@ninenines.eu>
 # This file is part of erlang.mk and subject to the terms of the ISC License.
 
-# Verbosity.
-
-dtl_verbose_0 = @echo " DTL   " $(filter %.dtl,$(?F));
-dtl_verbose = $(dtl_verbose_$(V))
-
-# Core targets.
-
-define compile_erlydtl
-	$(dtl_verbose) erl -noshell -pa ebin/ $(DEPS_DIR)/erlydtl/ebin/ -eval ' \
-		Compile = fun(F) -> \
-			Module = list_to_atom( \
-				string:to_lower(filename:basename(F, ".dtl")) ++ "_dtl"), \
-			erlydtl:compile(F, Module, [{out_dir, "ebin/"}]) \
-		end, \
-		_ = [Compile(F) || F <- string:tokens("$(1)", " ")], \
-		init:stop()'
-endef
-
-ifneq ($(wildcard src/),)
-ebin/$(PROJECT).app:: $(shell find templates -type f -name \*.dtl 2>/dev/null)
-	$(if $(strip $?),$(call compile_erlydtl,$?))
-endif
-
-# Copyright (c) 2013-2014, Loïc Hoguin <essen@ninenines.eu>
-# This file is part of erlang.mk and subject to the terms of the ISC License.
-
 .PHONY: distclean-edoc
 
 # Configuration.
@@ -690,52 +454,3 @@ distclean:: distclean-edoc
 
 distclean-edoc:
 	$(gen_verbose) rm -f doc/*.css doc/*.html doc/*.png doc/edoc-info
-
-# Copyright (c) 2013-2014, Loïc Hoguin <essen@ninenines.eu>
-# This file is part of erlang.mk and subject to the terms of the ISC License.
-
-.PHONY: relx-rel distclean-relx-rel distclean-relx
-
-# Configuration.
-
-RELX_CONFIG ?= $(CURDIR)/relx.config
-
-RELX ?= $(CURDIR)/relx
-export RELX
-
-RELX_URL ?= https://github.com/erlware/relx/releases/download/v1.0.2/relx
-RELX_OPTS ?=
-RELX_OUTPUT_DIR ?= _rel
-
-ifeq ($(firstword $(RELX_OPTS)),-o)
-	RELX_OUTPUT_DIR = $(word 2,$(RELX_OPTS))
-else
-	RELX_OPTS += -o $(RELX_OUTPUT_DIR)
-endif
-
-# Core targets.
-
-ifneq ($(wildcard $(RELX_CONFIG)),)
-rel:: distclean-relx-rel relx-rel
-endif
-
-distclean:: distclean-relx-rel distclean-relx
-
-# Plugin-specific targets.
-
-define relx_fetch
-	$(call core_http_get,$(RELX),$(RELX_URL))
-	chmod +x $(RELX)
-endef
-
-$(RELX):
-	@$(call relx_fetch)
-
-relx-rel: $(RELX)
-	@$(RELX) -c $(RELX_CONFIG) $(RELX_OPTS)
-
-distclean-relx-rel:
-	$(gen_verbose) rm -rf $(RELX_OUTPUT_DIR)
-
-distclean-relx:
-	$(gen_verbose) rm -rf $(RELX)
